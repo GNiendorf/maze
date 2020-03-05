2927b863b6773be079103f9548fd23856d155cd9
Gavin Niendorf <gavinniendorf@gmail.com>
2020-03-05 01:16:55 -0800

    Update maze.py

--- Maze/envs/maze.py
+++ Maze/envs/maze.py
@@ -19,33 +19,8 @@ class Tree(object):
     def connect(self, tree):
         tree.root().parent = self
 
-def display_maze(grid):
-    low = np.zeros((6,3,3), dtype=int)
-    empty = np.zeros((6,3,3), dtype=int)
-    wall = np.zeros((6,3,3), dtype=int)
-    low[3:,:,0] = 255
-    wall[:,:,0] = 255
-    maze = np.repeat(wall[3:,:,:],(len(grid[0]) * 2 + 1), axis=1)
-    for idx, row in enumerate(grid):
-        line = wall
-        for x, cell in enumerate(row):
-            if (cell & S != 0):
-                line = np.hstack((line, empty))
-            else:
-                line = np.hstack((line, low))
-            if cell & E != 0:
-                if ((cell | row[x+1]) & S != 0):
-                    line = np.hstack((line, empty))
-                else:
-                    line = np.hstack((line, low))
-            else:
-                line = np.hstack((line, wall))
-        maze = np.vstack((maze, line))
-    pad = int((64-np.shape(maze)[0])/2)
-    padded_pic = np.pad(maze, ((pad,pad),(pad,pad), (0,0)), mode='edge')
-    return padded_pic
-
-def generate_maze(size):
+def generate_maze(size, seed):
+    np.random.seed(seed)
     width=int((size+1)/2)
     height=int((size+1)/2)
     grid = [[0 for y in range(width)] for x in range(height)]
